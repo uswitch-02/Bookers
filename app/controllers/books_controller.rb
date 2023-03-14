32b87bc467df79e39e6a0637c16757009b282ddc
uswith <124918064+uswitch-02@users.noreply.github.com>
2023-03-14 08:36:54 +0000

    [update]編集画面の変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,12 @@
 class BooksController < ApplicationController
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
 
   def index
@@ -9,7 +16,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to list_path(list.id)
+    redirect_to book_path(book.id)
   end
 
   def show
