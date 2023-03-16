0f1c5cf63dddfec56ab28c38398ef284e65fc0ef
uswith <124918064+uswitch-02@users.noreply.github.com>
2023-03-16 06:13:17 +0000

    [update]bookers フラッシュメッセージ表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,12 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.save
+      redirect_to book_path(book.id)
+    else
+      @books = Book.all
+      render :edit
+    end
   end
 
   def index
@@ -16,13 +21,15 @@ class BooksController < ApplicationController
 
 
   def create
-    @book = Book.new(book_params)
-    if @book.save
-      redirect_to book_path(@book.id)
+    book = Book.new(book_params)
+    if book.save
+      flash[:notice] = "Book was succesfully created."
+       redirect_to book_path(book.id)
     else
       @books = Book.all
       render :index
     end
+     
   end
 
   def show
@@ -34,12 +41,13 @@ class BooksController < ApplicationController
     book.destroy
     redirect_to '/books'
   end
+  
+  def login
+    @book = Book.book_path
+  end  
 
   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
-
-
-
 end
